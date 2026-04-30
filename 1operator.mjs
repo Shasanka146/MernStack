3eec1a49e3f192cf230377f09f385a36d8f323b5
Shashanka Acharya <shasankaacharya14@gmail.com>
2026-04-30 13:09:31 +0545

    Arithmetic operation

---
+++ 1operator.mjs
@@ -0,0 +1,14 @@
+/* 
+1.Arithmetic operator
+2.Assignment operator
+3.Comparison operator
+4.Logical operator
+5.Type operator
+ */
+
+// 1.Arithmetic operator
+console.log(1 + 1);
+console.log(3- 1);
+console.log(9 * 1);
+console.log(1 / 1);
+console.log(1 & 1); // Addition
