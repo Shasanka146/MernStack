a2e654f93c5cf5bcbd2c23afdf4f86b3ee8a5b2a
Shashanka Acharya <shasankaacharya14@gmail.com>
2026-04-30 12:52:52 +0545

    variable in javascript

---
+++ shasank.mjs
@@ -0,0 +1,35 @@
+/*
+// console.log("Hello, I'm Shasank! ");
+console.log("I'm a developer with a passion for creating innovative solutions.");
+console.log(1);
+console.log(true);//it prints true in the console
+console.log(true);
+console.log(true); */
+
+
+
+
+/*
+//Data Types in JavaScript
+
+number 1,2,3,1.1,2.2,3.3
+string "Hello", 'World', `Template Literals`
+boolean true, false
+*/
+
+//variables in JavaScript
+//define variable
+let name = "Shasank";
+let age = 25;
+let isDeveloper = true;
+let hascar = false;
+let a=123
+let b=123
+let c=a+b
+
+//calling variable
+console.log(c); 
+console.log(name);
+console.log(age);
+console.log(isDeveloper);
+console.log(hascar);
