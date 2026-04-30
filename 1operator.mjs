c5bc67c287ff1cba29bfc1262b4d545695a53bb0
Shashanka Acharya <shasankaacharya14@gmail.com>
2026-04-30 13:18:30 +0545

    other operator

--- 1operator.mjs
+++ 1operator.mjs
@@ -7,8 +7,34 @@
  */
 
 // 1.Arithmetic operator
+/*
 console.log(1 + 1);
 console.log(3- 1);
 console.log(9 * 1);
 console.log(1 / 1);
 console.log(1 & 1); // Addition
+*/
+
+
+
+// 2.Assignment operator
+/*let a = 10;
+a += 5; // a = a + 5
+console.log(a);   
+*/
+
+//Comparision operator
+/*
+console.log(3> 2); // true
+console.log(3< 2); // false
+console.log(3 === 3); // true
+console.log(3 !== 3); // false
+*/
+
+//logical operator
+/*
+// && (AND), || (OR), ! (NOT)
+console.log(!true); // false
+console.log(true && false); // false
+console.log(true || false); // true
+*/
