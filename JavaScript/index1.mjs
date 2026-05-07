9067b6872a5f2737f46955fd5229cb19a5703eb2
Shashanka Acharya <shasankaacharya14@gmail.com>
2026-05-07 12:36:36 +0545

    getting information from another file

---
+++ JavaScript/index1.mjs
@@ -0,0 +1,13 @@
+//export by name which can be used unlimitely 
+/* export let student= {
+    name:"shasank",
+    age:19,
+    weight:55,
+    isMarried:true
+}
+//export by default which can be used only once
+let address={city:"kathmandu"}
+export default address;
+
+ */
+
---
+++ JavaScript/index2.mjs
@@ -0,0 +1,4 @@
+/* import address, { student } from "./index1.mjs";
+
+console.log(student);
+console.log(address); */
